1d8f4c52eb7ea6d5045e02b7393a69800927f585
narozhniakmarian <narozhniakmarian@gmail.com>
2025-05-18 16:06:12 +0200

    lesson1

--- js/task-1.js
+++ js/task-1.js
@@ -1,10 +1,195 @@
-function makeTransaction(quantity, pricePerDroid) {
-    const totalPrice = quantity * pricePerDroid;
-     return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
-}
+/* // Запропонуйте користувачу ввести число за допомогою prompt().
+// Збережіть введене значення у змінну number.
+// Перевірте, чи дорівнює змінна number числу 10:
+//    Якщо так – виведіть повідомлення 'Вірно' у alert().
+//    Якщо ні – виведіть 'Невірно'.
+
+// Підказка: prompt завжди повертає рядок (String), тому перед перевіркою
+// перетворіть значення на число за допомогою Number().
+*/
+
+// function checkNumber(paramtr) {
+//     const number = Number(prompt("ВВедіть число"));
+//     if (number === 10) {
+//         alert("ok");
+//     } else {
+//         alert("nok");
+//     }
+// }
+// checkNumber();
+
+/*// У змінній min лежить випадкове число від 0 до 59.
+// Визначте, в яку чверть години потрапляє
+// це число (у першу, другу, третю чи четверту).
+// Виведіть в alert відповідне повідомлення, наприклад:
+// "10 входить в першу чверть"
+
+// const min = Math.floor(Math.random() * (59 - 0) + 0);
+ */
+
+// function checkHour() {
+//     let min = Number(prompt("Котра година?"));
+
+//     if (min <= 15 && min > 0) {
+//         alert(`${min} входить в першу чверть`);
+//     } else if (min <= 30 && min > 15) {
+//         alert(`${min} входить в другу чверть`);
+//     } else if (min <= 45 && min > 30) {
+//         alert(`${min} входить в третю чверть`);
+//     } else if (min <= 60 && min > 45) {
+//         alert(`${min} входить в четверту чверть`);
+//     } else {
+//         alert("Заміни батерейку");
+//     }
+// }
+// checkHour();
 
 
-console.log(makeTransaction(5, 3000));
-console.log(makeTransaction(3, 1000));
-console.log(makeTransaction(10, 500));
+/*// Змінна num може набувати 4 значення: '1', '2', '3' або '4'
+// (запитуй це значення у користувача через prompt).
+// Якщо вона має значення '1', то у змінну result запишіть 'зима',
+// якщо має значення '2' - 'весна' і так далі.
+// Розв'яжіть завдання через switch-case.
+// Не забудьте про дефолтне значення, на випадок, якщо користувач
+// введе в prompt щось інше. В такому випадку result має набувати значення:
+// "Вибачте, але ви маєте ввести значення від 1 до 4 включно".
+// Значення змінної result виведіть в консоль.
+ */
 
+// function season() {
+//     const userPromt = Number(prompt("ВВедіть від 1 до 4"));
+//     let result;
+//     switch (userPromt) {
+//         case 1:
+//             result = 'зима';
+//             break;
+//         case 2:
+//             result = 'весна';
+//             break;
+//         case 3:
+//             result = 'літо';
+//             break;
+//         case 4:
+//             result = 'осінь';
+//             break;
+//         default:
+//             result = "Вибачте, але ви маєте ввести значення від 1 до 4 включно";
+//     }
+//     alert(result);
+// }
+// season();
+
+/*// Отримуйте від користувача число (кількість хвилин) через prompt
+// і виводьте у консоль рядок у форматі годин та хвилин.
+// Приклад: користувач вводить в prompt '70' ->
+// в консолі відобразиться '01:10'.
+// Корисне посилання для відображення годин та хвилин
+//  у потрібному форматі ('01' замість '1'):
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/padStart#examples
+ */
+
+//*console.log(("1".padStart(2, "0")));
+
+// function timeCalc() {
+//     const minuteTotal = Number(prompt("Введіть кількість хвилин:"));
+//     let hours = 0;
+//     let minutes = minuteTotal;
+
+//     if (minutes < 0) {
+//         return `${minutes} не коректний час`
+//     }
+//     while (minutes >= 60) {
+//         hours = hours + 1;
+//         minutes = minutes - 60;
+//     }
+//     const hourFormat = String(hours).padStart(2, '0');
+//     const minuteFormat = String(minutes).padStart(2, '0');
+
+//     return hourFormat + ':' + minuteFormat;
+// }
+
+// console.log(timeCalc());
+
+/*// Напишіть код, який запитуватиме у користувача
+// логін за допомогою prompt і виводить результат в консоль браузера
+
+// Після цього додайте перевірку введеного користувачем значення:
+// Якщо відвідувач вводить "Адмін",
+// то prompt запитує пароль
+// (не забудьте зберігти його у змінну для подальшої перевірки).
+// Якщо нічого не ввели або натиснули Cancel,
+// Вивести в alert строку "Скасовано"
+// В іншому випадку вивести в alert рядок "Я вас не знаю"
+
+// Пароль перевіряти так:
+// Якщо введено пароль "Я головний",
+// то вивести в alert рядок "Добрий день!",
+// в іншому випадку вивести в alert рядок "Невірний пароль!"
+ */
+
+// function userSelect() {
+//     const user = prompt('Введіть логін користувача');
+//     console.log(user);
+
+//     if (user === null) {
+//         alert('Скасовано');
+//         return;
+//     }
+
+//     if (user.toLowerCase() === "адмін") {
+//         const pass = prompt('Введіть пароль користувача');
+//         console.log(pass);
+
+//         if (pass === null) {
+//             alert('Скасовано');
+//         } else if (pass.toLowerCase() === "я головний") {
+//             alert('Добрий день!');
+//         } else {
+//             alert('Невірний пароль!');
+//         }
+//     } else {
+//         alert('Скасовано');
+//     }
+// }
+
+// userSelect();
+
+
+/*// Напишіть функцію getNumbers(min, max), що приймає 2 параметри -
+// мінімальне і максимальне число відповідно.
+// Напишіть цикл всередині функції, який виводить у консоль
+// всі числа від max до min за спаданням.
+// Окрім цього, підрахуйте суму всіх парних чисел в цьому циклі
+// і поверніть її з функції.
+ */
+
+// function getNumbers(min, max) {
+//     let sum = 0;
+//     for (let i = max; i > min; i--) {
+//         console.log(i);
+//         if (i % 2 === 0) {
+//             sum += i;
+//         }
+//     }
+//     return sum;
+// }
+// getNumbers(15, 30);
+
+/*// Напишіть функцію min(a, b), яка приймає 2 числа і повертає
+//  меньше з них.
+// Потрібно додати перевірку, що функція отримує саме числа,
+//  в іншому випадку
+// повертати з функції рядок - 'Not a number!'.
+ */
+
+// function min(a, b) {
+//     if (typeof a !== 'number' || typeof b !== 'number') {
+//         return 'Not a number!';
+//     }
+
+//     if (a < b) {
+//         return a;
+//     } else {
+//         return b;
+//     }
+// }
